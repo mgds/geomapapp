9171fb645ee1de14d90469455e0476f61fedb9f6
astrong-ldeo <astrong@ldeo.columbia.edu>
2025-08-12 11:14:38 -0400

    Committing to test new github repo

--- haxby/map/MapApp.java
+++ haxby/map/MapApp.java
@@ -8,6 +8,7 @@ import java.awt.Dimension;
 import java.awt.FlowLayout;
 import java.awt.Font;
 import java.awt.Frame;
+import java.awt.GraphicsDevice;
 import java.awt.GraphicsEnvironment;
 import java.awt.GridBagLayout;
 import java.awt.GridBagConstraints;
@@ -16,6 +17,7 @@ import java.awt.Insets;
 import java.awt.Point;
 import java.awt.Rectangle;
 import java.awt.Toolkit;
+import java.awt.Window;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.AdjustmentEvent;
@@ -5252,6 +5254,15 @@ public class MapApp implements ActionListener,
 		catch(Exception e) {
 			e.printStackTrace();
 		}
+	}
+	
+	public static int getGraphicsDevice(Component c) {
+		GraphicsDevice[] screenDevices = GraphicsEnvironment.getLocalGraphicsEnvironment().getScreenDevices();
+		GraphicsDevice compDevice = c.getGraphicsConfiguration().getDevice();
+		for(int i = 0; i < screenDevices.length; i++) {
+			if(compDevice.equals(screenDevices[i])) return i;
+		}
+		return -1;
 	}
 	
 	public static HashSet<String> supported_commands = new HashSet<String>();
